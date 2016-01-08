0cb7d1b659dc07172557787a47439cf9b01d3f9d
Peiliang Guo <edwardplguo@gmail.com>
2016-01-08 17:25:22 -0500

    Fix app.js

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var port = process.env.PORT || 8080;
+
 app.get('/', function(req, res){
   res.send('<h1>Hello world</h1>');
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+app.listen(port, function() {
+    console.log('App is running on http://localhost:' + port);
 });
